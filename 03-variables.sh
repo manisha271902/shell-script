d71ef1fb6a478e19acfd755de9c90fff4cb43ca9
manisha271902 <manishachennoju97@gmail.com>
2024-04-20 23:33:14 +0530

    First shell script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 person1=manisha
-person2=manisha's world
+person2=world
 echo "$person1:  Hey $person2 Iam leaving to us in october"
 echo "$person2 : Wowww  $person1 Just go and rock it"
 
