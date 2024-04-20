cdbe76a7e0d663b1dee13e50f73c432d5930b317
manisha271902 <manishachennoju97@gmail.com>
2024-04-20 23:24:30 +0530

    First shell script

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,18 +1,7 @@
-# #!/bin/bash
-# person1=chitti
-# person2=family
-# echo "$person1 :  Hey $person2 Iam leaving to us in october"
-# echo "$person2 : Wowww  $person1 Just go and rock it"
-
-
 #!/bin/bash
+person1=chitti
+person2=family
+echo "$person1:  Hey $person2 Iam leaving to us in october"
+echo "$person2 : Wowww  $person1 Just go and rock it"
 
-#declaring of varible
-PERSON1=Sachin #no space between = and value
-PERSON2=Rahul
 
-#referring variable
-echo "$PERSON1:: Hello $PERSON2, How are you?"
-echo "${PERSON2}:: Hi $PERSON1, I am fine. How are you?"
-echo "$PERSON1:: I am fine too. how is your work?"
-echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
