d3f58bbea0bcaddbf43a559cd9966cb16028f8ee
manisha271902 <manishachennoju97@gmail.com>
2024-04-21 01:25:37 +0530

    some-message

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-person1=manisha
+person1=manishachennoju
 person2=srinulathacherrypoojas
 echo "$person1:  Hey $person2 Iam leaving to us in october"
 echo "$person2 : Wowww  $person1 Just go and rock it"
---
+++ 04-variables.sh
@@ -0,0 +1,4 @@
+Name1=$1
+Name2=$2
+
+echo "$Name1 : $Name1 and $Name2 are best friends"
