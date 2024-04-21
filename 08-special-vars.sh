39d9c597fe232c6a563fed277da528094d3ae95b
manisha271902 <manishachennoju97@gmail.com>
2024-04-22 00:46:58 +0530

    some-message

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -9,4 +9,4 @@ echo "WHich user is running this script : $USER"
 echo "Hostname : $HOSTNAME"
 echo "PRocess Id of the current shell script: $$"
 sleep 60 & #cmnd ni background ki papiyali ante &
-echo "Process ID of the last background command : $!"
+echo "Process ID of the last background command : $!" 
---
+++ 09-conditions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$NUMBER=10
+
+if [ $NUMBER gt 50]
+then
+    echo "Given numvber $NUMBER is greaterthan 50"
+else
+    echo " GIven num,ber is less than 50"
+fi
