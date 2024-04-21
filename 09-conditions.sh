9e0f19893cfa0bff6d6dc74be29d7bda974c100c
manisha271902 <manishachennoju97@gmail.com>
2024-04-22 00:48:47 +0530

    some-message

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,9 +2,11 @@
 
 $NUMBER=10
 
-if [ $NUMBER gt 50]
+if [ $NUMBER -gt 50 ]
 then
     echo "Given numvber $NUMBER is greaterthan 50"
 else
     echo " GIven num,ber is less than 50"
 fi
+
+#-gt, -lt, eq, -ge, -le
