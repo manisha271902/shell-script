2b49cca01ae659e2abf771777b8c760d65d48ef3
manisha271902 <manishachennoju97@gmail.com>
2024-04-25 11:53:50 +0530

    some-msg

--- 10-installations.sh
+++ 10-installations.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 USERID=$(id -u) #nospcae here
-
-if [ $USERID -ne 0 ]
+if [ $USERID -ne 0 ] #ikada space lekunte error ochindhi
 then
-    echo "Please run the script with root user acacess"
-    exit 1
+    echo "Please run  the script as root user"
+    exit 1 #manually exit if error comes
 else
-    echo "youare a super user" 
-
-VALIDATE(){
-     echo "exit status : $1"
-     echo "HEYAA what are youdoing. : $2"
-}
+    echo "You are a super user"
+fi
 
 
 
@@ -19,10 +14,24 @@ VALIDATE(){
 
 
 dnf install mysql -y
-VALIDATE $? "Installing SQL"
+
+if [ $? -ne 0 ]
+then 
+    echo "installlation of mysql is not done"
+    exit 1
+else
+    echo "installation of mysql is success"
+
+fi
 
 
 
 dnf install nginx -y
-VALIDATE $? "Installing nginx"
+if [ $? -ne 0 ]
+then
+    echo "Please run as super user"
+else
+    echo "INstall;ation of nginx is done"
 
+
+echo "is script proceeding"
---
+++ 11-functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+USERID=$(id -u) #nospcae here
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run the script with root user acacess"
+    exit 1
+else
+    echo "youare a super user" 
+
+VALIDATE(){
+     echo "exit status : $1"
+     echo "HEYAA what are youdoing. : $2"
+}
+
+
+
+
+
+
+dnf install mysql -y
+VALIDATE $? "Installing SQL"
+
+
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx"
