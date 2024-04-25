23aeaaee84ae24752ca22ef8581a267a2d4dc6f8
manisha271902 <manishachennoju97@gmail.com>
2024-04-25 08:24:20 +0530

    some-msg

--- 10-installations.sh
+++ 10-installations.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-USERID=$(id -u)
-if [ $USERID -ne 0 ]
+USERID=$(id -u) #nospcae here
+if [ $USERID -ne 0 ] #ikada space lekunte error ochindhi
 then
     echo "Please run  the script as root user"
-    exit 1
+    exit 1 #manually exit if error comes
 fi
-dnf install mysql -y
+dnf install mysqll -y
 
 echo "is script proceeding?"
+
+if [ $1? -ne 0 ]
+then 
+    echo "installlation of mysql is not done"
+    exit 1
+else
+    echo "installation success"
