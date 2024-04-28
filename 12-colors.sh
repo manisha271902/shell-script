f4ea6dc6353bfb3c3461bc939578c09f582ba8c9
manisha271902 <manishachennoju97@gmail.com>
2024-04-28 19:10:35 +0530

    shell script

--- 12-colors.sh
+++ 12-colors.sh
@@ -17,9 +17,9 @@ fi
 VALIDATE(){
      if [ $1 -ne 0 ] 
      then
-         echo -e  "$2 ...$R FAILED!!!!!!"
+         echo -e  "$2 ...$R FAILED!!!!!! $N"
      else
-         echo -e "$2 ........$G SUCCESS!!!!!!!"
+         echo -e "$2 ........$G SUCCESS!!!!!!! $N"
      fi
 }
 
