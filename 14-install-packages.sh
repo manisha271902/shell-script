721c2e0126670d4af664048eac9358c897de8ea0
manisha271902 <manishachennoju97@gmail.com>
2024-05-04 17:49:41 +0530

    shell script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
-$USERID=$(id -u)
-$TIMESTAMP=$(date +%F-%H-%M-%S)
-$SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-$LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
