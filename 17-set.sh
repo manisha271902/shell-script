8316e71dd72f074f06f15c3c71756bff9cf116c7
manisha271902 <manishachennoju97@gmail.com>
2024-05-19 02:30:28 +0530

    shell

---
+++ 17-set.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e 
+
+
+failure() {
+#   local lineno=$1
+#   local msg=$2
+  echo "Failed at $1: $2"
+}
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u) #nospcae here
+if [ $USERID -ne 0 ] #ikada space lekunte error ochindhi
+then
+    echo "Please run  the script as root user"
+    exit 1 #manually exit if error comes
+else
+    echo "You are a super user"
+fi
+
+dnf install mysql -y
+
+dnf install nginx -y
+
+echo "is script proceeding"
---
+++ 18-delete-old-log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SOURCE_DIR=/tmp/app-logs
+
+if [ -d $SOURCE_DIR ]
+then
+    echo "SOurce direcoryt exists"
+else
+    echo "SOurce direcory does note xists"
+    exit 1
+fi
+
+FILES=$(find SOURCE_DIR -name "*log" -mtime +14)
+
+while IFS=read -r line
+do
+  echo "List of files are $line"   #line anedhi seperator ikada, - ila different vi untay
+  rm -rf $line
+done <<< $FILES
