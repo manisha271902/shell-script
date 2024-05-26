c1a046ab14bf9362bd3c48e5c52d2900485b7c6d
manisha271902 <manishachennoju97@gmail.com>
2024-05-26 18:41:35 +0530

    shell

--- 20-Ass-1.sh
+++ 20-Ass-1.sh
@@ -4,12 +4,14 @@
 #script that reads text file and counts the occurences of each word ,
 # displaying the top 5 most frequent words along with their counts
 
-FILE_NAME="/home/centos/shell-script/sample.txt"
+FILE_NAME="sample.txt"
 
-while IFS= read -r line
-do 
-    echo "$line"
-
-
-
-done < $FILE_NAME
+if [ -d $FILE_NAME ]
+then
+    while IFS= read -r line
+    do 
+        echo "$line"
+    done < $FILE_NAME
+else
+    echo "File does not exixts"
+fi
