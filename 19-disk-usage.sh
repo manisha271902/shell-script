b6dca36342e6ea171701ccc88f2bc2fe242f2296
manisha271902 <manishachennoju97@gmail.com>
2024-05-26 17:58:32 +0530

    shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -6,7 +6,7 @@ MESSAGE=""
 
 while read -s line 
 do
-    USAGE=$(echo $line | awk -F " " 'print{$6F}' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then
