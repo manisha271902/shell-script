1c77742b1a8ec83462da9f3bf0b960dc54567bec
manisha271902 <manishachennoju97@gmail.com>
2024-05-26 17:40:24 +0530

    shell

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-
 DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=6
+
+MESSAGE=""
+
+while read -s line 
+do
+    USAGE=$(echo $line | awk -F " " 'print{$6F}' | cut -d "%" -f1)
+    FOLDER=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MESSAGE="$FOLDER is more than $DISK_THRESHOLD , current usage is : $USAGE"
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message: $MESSAGE"
+
+echo "$MESSAGE" | mail -s "Disk Usage Alert" manishachennoju97@gmail.com
+
+# echo "body" | mail -s "subject" to-address
