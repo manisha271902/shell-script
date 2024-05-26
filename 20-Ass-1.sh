404dbbe6d5d09ed40838e49370ae0e680a2f35b5
manisha271902 <manishachennoju97@gmail.com>
2024-05-26 19:52:45 +0530

    shell

--- 20-Ass-1.sh
+++ 20-Ass-1.sh
@@ -5,7 +5,7 @@
 # displaying the top 5 most frequent words along with their counts
 
 FILE_NAME="/home/centos/shell-script/sample.txt"
-
+echo "File path: $FILE_NAME"
 if [ -f $FILE_NAME ]
 then
     while IFS= read -r line
