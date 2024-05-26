ab813433f669c176164d2f65aefa8d31e2f3e76c
manisha271902 <manishachennoju97@gmail.com>
2024-05-26 18:38:24 +0530

    shell

--- 20-Ass-1.sh
+++ 20-Ass-1.sh
@@ -4,7 +4,7 @@
 #script that reads text file and counts the occurences of each word ,
 # displaying the top 5 most frequent words along with their counts
 
-FILE_NAME="C:\Users\manis\Devopsclass\repos\shell-script\sample.txt"
+FILE_NAME="repos\shell-script\sample.txt"
 
 while IFS= read -r line
 do 
