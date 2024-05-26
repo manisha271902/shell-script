93e90ffde43de418e79baa7d3a2e8456784878bb
manisha271902 <manishachennoju97@gmail.com>
2024-05-26 19:58:19 +0530

    shell

--- 20-Ass-1.sh
+++ 20-Ass-1.sh
@@ -8,9 +8,9 @@ FILE_NAME="/home/centos/shell-script/sample.txt"
 echo "File path: $FILE_NAME"
 if [ -f $FILE_NAME ]
 then
-    while IFS= read -r line
+    while read line
     do 
-       echo $FILE_NAME
+       echo $line
     done < "$FILE_NAME"
 else
     echo "File does not exixts"
