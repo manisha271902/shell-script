7a3614ac906869d6c886f547e6b1d89335d954ec
manisha271902 <manishachennoju97@gmail.com>
2024-05-28 00:52:58 +0530

    Shell-Assignment

--- 20-Ass-1.sh
+++ 20-Ass-1.sh
@@ -5,20 +5,19 @@
 # displaying the top 5 most frequent words along with their counts
 
 FILE_NAME="/home/centos/shell-script/sample.txt"
-s=$(cat $FILE_NAME)
-echo $s
-# echo "File path: $FILE_NAME"
-# if [ -f $FILE_NAME ]
-# then
-#     while IFS= read line
-#     do 
+
+echo "File path: $FILE_NAME"
+if [ -f $FILE_NAME ]
+then
+    while IFS= read line
+    do 
        
-#        echo $line
+       echo $(cat $FILE_NAME)
        
-#     done < "$FILE_NAME"
-# else
-#     echo "File does not exixts"
-# fi
+    done < "$FILE_NAME"
+else
+    echo "File does not exixts"
+fi
 
 
 
