4f8084d9f298f32859f84d8386e5a4ffd8db21db
manisha271902 <manishachennoju97@gmail.com>
2024-05-28 02:03:14 +0530

    Shell-Assignment

--- 20-Ass-1.sh
+++ 20-Ass-1.sh
@@ -9,10 +9,11 @@ FILE_NAME="/home/centos/shell-script/sample.txt"
 echo "File path: $FILE_NAME"
 if [ -f $FILE_NAME ]
 then
-    s=$(cat $FILE_NAME)  
+    
     # echo $s
-    clean_line=$(echo "$s" | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | tr -s ' ' '\n' | sort) 
-    echo $clean_line
+    cat $FILE_NAME | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | tr -s ' ' '\n' | sort
+    #paina varaku induvidual words laga sort ayi untadi
+    # echo $clean_line
         # for i in $clean_line
         # do
            
