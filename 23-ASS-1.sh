2647d38fe899d81ae4e4da1b0cebe44fe37c8cea
manisha271902 <manishachennoju97@gmail.com>
2024-11-26 22:07:06 +0530

    shell

--- 23-ASS-1.sh
+++ 23-ASS-1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-words=$(awk '{for (i=1;i<=NF;i++) word-count[$i]++} END {for (i in word-count) print word, word-count[i]}' sample.txt) 
+words=$(awk '{for (i=1;i<=NF;i++) word_count[$i]++} END {for (i in word_count) print word, word-count[i]}' sample.txt) 
 echo "$words"
 
 # tr -s '[:space:]' '\n' < sample.txt
