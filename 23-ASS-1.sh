fb6bb2feb31cf494ff7821834a6fec42e9fcf85b
manisha271902 <manishachennoju97@gmail.com>
2024-11-26 12:40:21 +0530

    Functions

--- 23-ASS-1.sh
+++ 23-ASS-1.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 file_name="sample.txt"
-for i in file_name
-do
-    echo $i
-    words=$(awk -F " " '{for (i=1;i<=NF;i++) sample.txt}')
+words=$(awk -F " " '{for (i=1;i<=NF;i++) sample.txt}')
 
-done <<< $file_name
