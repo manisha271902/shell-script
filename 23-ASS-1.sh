50a4db0557aa83fea6f190cf3744a6d6a3c2f763
manisha271902 <manishachennoju97@gmail.com>
2024-11-26 12:36:20 +0530

    Functions

--- 23-ASS-1.sh
+++ 23-ASS-1.sh
@@ -3,6 +3,6 @@ file_name="sample.txt"
 while IFS= read -r line
 do
     echo $(cat $line)
-    words=$(awk -F " " '{for i=1;i<=NF;i++} print $i')
-    
+    words=$(awk -F " " '{for i=1;i<=NF;i++ print $i}')
+
 done <<< $file_name
