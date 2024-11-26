70302b53fcaa3327a06946751af2948a5243a4d0
manisha271902 <manishachennoju97@gmail.com>
2024-11-26 12:22:57 +0530

    Functions

--- 20-Ass-1.sh
+++ 20-Ass-1.sh
@@ -4,15 +4,15 @@
 #script that reads text file and counts the occurences of each word ,
 # displaying the top 5 most frequent words along with their counts
 
-FILE_NAME="/home/centos/shell-script/sample.txt"
+# FILE_NAME="/home/centos/shell-script/sample.txt"
 
-echo "File path: $FILE_NAME"
-if [ -f $FILE_NAME ]
-then
-    cat $FILE_NAME | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | tr -s ' ' '\n' | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2,$1}' 
-else
-    echo "File does not exixts"
-fi
+# echo "File path: $FILE_NAME"
+# if [ -f $FILE_NAME ]
+# then
+#     cat $FILE_NAME | tr -d '[:punct:]' | tr '[:upper:]' '[:lower:]' | tr -s ' ' '\n' | sort | uniq -c | sort -nr | head -n 5 | awk '{print $2,$1}' 
+# else
+#     echo "File does not exixts"
+# fi
 
 
 
@@ -30,3 +30,22 @@ fi
         # done
     # done < $FILE_NAME
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 23-ASS-1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+file_name="sample.txt"
+
+while IFS=" " read -r line;
+do
+    echo $line
+
+done <<< $file_name
