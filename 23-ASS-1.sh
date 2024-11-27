3b2ca6bc90bb8cb4bd5d9620554610ac0849c450
manisha271902 <manishachennoju97@gmail.com>
2024-11-27 12:16:14 +0530

    shell

--- 23-ASS-1.sh
+++ 23-ASS-1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-awk '{for (i=1;i<=NF;i++) word_c[$i]++} END {for (i in word_c) print i, word_c[i]}' sample.txt | sort -k2,2nr | head -n 5
+# awk '{for (i=1;i<=NF;i++) word_c[$i]++} END {for (i in word_c) print i, word_c[i]}' sample.txt | sort -k2,2nr | head -n 5
 
 # echo "$words"
 
@@ -7,3 +7,5 @@ awk '{for (i=1;i<=NF;i++) word_c[$i]++} END {for (i in word_c) print i, word_c[i
 
 
 
+w=awk '{for (i=1;i<=NF;i++) word_c[$i]++}'
+echo "$w"
