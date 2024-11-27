469dd5f6422a5efeca6a6fa201729673cf479a22
manisha271902 <manishachennoju97@gmail.com>
2024-11-27 13:44:52 +0530

    shell

--- 26-ASS-4.sh
+++ 26-ASS-4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-awk '{ for(i=1; i<=NF; i++) a[i]=a[i]} END {for(i=1; i<=NF; i++) print a[i]}' ass.txt
+awk '{ for(i=1; i<=NF; i++) a[i]=a[i] $i} END {for(i=1; i<=NF; i++) print a[i]}' ass.txt
 
 
