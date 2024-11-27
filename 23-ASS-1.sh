8ee291aeb3cacd2c9780adfbbbd7ee214787acdc
manisha271902 <manishachennoju97@gmail.com>
2024-11-27 13:05:36 +0530

    shell

--- 23-ASS-1.sh
+++ 23-ASS-1.sh
@@ -1,5 +1,11 @@
+#script that reads text file and counts the occurences of each word ,
+# displaying the top 5 most frequent words along with their counts
+
+
+
+
 #!/bin/bash
-# awk '{for (i=1;i<=NF;i++) word_c[$i]++} END {for (i in word_c) print i, word_c[i]}' sample.txt | sort -k2,2nr | head -n 5
+awk '{for (i=1;i<=NF;i++) word_c[$i]++} END {for (i in word_c) print i, word_c[i]}' sample.txt | sort -k2,2nr | head -n 5
 
 # echo "$words"
 
@@ -7,5 +13,5 @@
 
 
 
-echo "$(awk '{for (i=1;i<=NF;i++) word_c[$i]++} END {for (i in word_c) print i, word_c[i]}' sample.txt)"
+# echo "$(awk '{for (i=1;i<=NF;i++) word_c[$i]++} END {for (i in word_c) print i, word_c[i]}' sample.txt)"
 
---
+++ 26-ASS-4.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo $(rs -Tzc < ass.txt)
